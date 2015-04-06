ac087ebb800791ca49f5ffee10099062e03c0896
Andreas Noack <andreasnoackjensen@gmail.com>
2015-04-06 16:04:37 -0700

    First lines

--- src/Elemental.jl
+++ src/Elemental.jl
@@ -1,5 +1,17 @@
 module Elemental
 
-# package code goes here
+const libEl = "/opt/lib/libEl"
+
+using MPI
+
+type DistSparseMatrix{T} <: AbstractMatrix{T}
+	obj::Ptr{Void}
+end
+
+function DistSparseMatrix(::Type{Float64}, comm::MPI.Comm = MPI.COMM_WORLD)
+	obj = Ptr{Void}[0]
+	ret = ccall((:ElDistSparseMatrixCreate_d, libEl), Cuint, (Ptr{Void}, Ptr{Cint}), &obj, &comm)
+	return obj[1]
+end
 
 end # module
