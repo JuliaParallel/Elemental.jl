818a42317bdf0eb61063bacc51183371738b3332
Andreas Noack <andreasnoackjensen@gmail.com>
2015-04-07 09:09:40 -0700

    Wrap more code for LAV.py example

--- src/Elemental.jl
+++ src/Elemental.jl
@@ -50,17 +50,17 @@ for (elty, ext) in ((:Float32, :s),
                     (:Complex64, :c),
                     (:Complex128, :z))
     @eval begin
-        function DistSparseMatrix(::Type{$elty}, comm = MPI_COMM_WORLD)
+        function DistSparseMatrix(::Type{$elty}, comm = MPI.COMM_WORLD)
             obj = Ref{Ptr{Void}}(C_NULL)
             err = ccall(($(string("ElDistSparseMatrixCreate_", ext)), libEl), Cuint, 
                 (Ref{Ptr{Void}}, Cint),
-                obj, comm[])
+                obj, comm.val)
             return DistSparseMatrix{$elty}(obj[])
         end
 
-        function DistSparseMatrix(::Type{$elty}, m::Integer, n::Integer, comm = MPI_COMM_WORLD)
+        function DistSparseMatrix(::Type{$elty}, m::Integer, n::Integer, comm = MPI.COMM_WORLD)
             A = DistSparseMatrix($elty, comm)
-            DistSparseMatrixResize(A, m, n)
+            resize(A, m, n)
             return A
         end
 
@@ -71,28 +71,405 @@ for (elty, ext) in ((:Float32, :s),
             err
         end
 
-        function DistSparseMatrixResize{$elty}(A::DistSparseMatrix{$elty}, height::Integer, width::Integer)
-            @show err = ccall(($(string("ElDistSparseMatrixResize_", ext)), libEl), Cuint,
+        function resize{$elty}(A::DistSparseMatrix{$elty}, height::Integer, width::Integer)
+            err = ccall(($(string("ElDistSparseMatrixResize_", ext)), libEl), Cuint,
                 (Ptr{Void}, ElInt, ElInt), 
                 A.obj, height, width)
             return A
         end
 
-        function DistSparseMatrixLocalHeight{$elty}(A::DistSparseMatrix{$elty})
-            height = Ref{ElInt}(0)
+        function localHeight{$elty}(A::DistSparseMatrix{$elty})
+            i = Ref{ElInt}(0)
             err = ccall(($(string("ElDistSparseMatrixLocalHeight_", ext)), libEl), Cuint,
                 (Ptr{Void}, Ref{ElInt}), 
-                A.obj, height)
-            return convert(Int, height[])
+                A.obj, i)
+            return i[]
         end
 
+        function reserve{$elty}(A::DistSparseMatrix{$elty}, numEntries::Integer)
+            err = ccall(($(string("ElDistSparseMatrixReserve_", ext)), libEl), Cuint,
+                (Ptr{Void}, ElInt), 
+                A.obj, numEntries)
+            return nothing
+        end
 
+        function globalRow{$elty}(A::DistSparseMatrix{$elty}, iLoc::Integer)
+            i = Ref{ElInt}(0)
+            err = ccall(($(string("ElDistSparseMatrixGlobalRow_", ext)), libEl), Cuint,
+                (Ptr{Void}, ElInt, Ref{ElInt}), 
+                A.obj, iLoc, i)
+            return i[]
+        end
+
+        function queueLocalUpdate{$elty}(A::DistSparseMatrix{$elty}, localRow::Integer, col::Integer, value::$elty)
+            err = ccall(($(string("ElDistSparseMatrixQueueLocalUpdate_", ext)), libEl), Cuint,
+                (Ptr{Void}, ElInt, ElInt, $elty), 
+                A.obj, localRow, col, value)
+            return nothing
+        end
+
+        function makeConsistent{$elty}(A::DistSparseMatrix{$elty})
+            err = ccall(($(string("ElDistSparseMatrixMakeConsistent_", ext)), libEl), Cuint,
+                (Ptr{Void},), 
+                A.obj)
+            return nothing
+        end
+
+        function height{$elty}(A::DistSparseMatrix{$elty})
+            i = Ref{ElInt}(0)
+            err = ccall(($(string("ElDistSparseMatrixHeight_", ext)), libEl), Cuint,
+                (Ptr{Void}, Ref{ElInt}), 
+                A.obj, i)
+            return i[]
+        end
+
+        function width{$elty}(A::DistSparseMatrix{$elty})
+            i = Ref{ElInt}(0)
+            err = ccall(($(string("ElDistSparseMatrixWidth_", ext)), libEl), Cuint,
+                (Ptr{Void}, Ref{ElInt}), 
+                A.obj, i)
+            return i[]
+        end
+
+        function comm(A::DistSparseMatrix{$elty})
+            cm = MPI.COMM_WORLD
+            rcm = Ref{Cint}(cm.val)
+            err = ccall(($(string("ElDistSparseMatrixComm_", ext)), libEl), Cuint,
+                (Ptr{Void}, Ref{Cint}), 
+                A.obj, rcm)
+            return cm
+        end
     end
 end
 
-# size(A::DistSparseMatrix) = (DistSparseMatrixLocalHeight(A), (DistSparseMatrixLocalHeight(A)))
-size(A::DistSparseMatrix) = (5,5)
 
+
+
+size(A::DistSparseMatrix) = (Int(height(A)), Int(width(A)))
+# size(A::DistSparseMatrix) = (5,5)
+
+# function show(io::IO, A::DistSparseMatrix)
+
+# end
+
+# DistMultiVec
+type DistMultiVec{T} <: AbstractVector{T}
+    obj::Ptr{Void}
+end
+
+for (elty, ext) in ((:Float32, :s),
+                    (:Float64, :d), 
+                    (:Complex64, :c),
+                    (:Complex128, :z))
+    @eval begin
+        function DistMultiVec(::Type{$elty}, comm = MPI.COMM_WORLD)
+            obj = Ref{Ptr{Void}}(C_NULL)
+            err = ccall(($(string("ElDistMultiVecCreate_", ext)), libEl), Cuint, 
+                (Ref{Ptr{Void}}, Cint),
+                obj, comm.val)
+            return DistMultiVec{$elty}(obj[])
+        end
+
+        # function DistMultiVec(::Type{$elty}, m::Integer, n::Integer, comm = MPI.COMM_WORLD)
+        #     obj = Ref{Ptr{Void}}(C_NULL)
+        #     err = ccall(($(string("ElDistMultiVecCreate_", ext)), libEl), Cuint, 
+        #         (Ref{Ptr{Void}}, Cint),
+        #         obj, comm.val)
+        #     return DistMultiVec{$elty}(obj[])
+        # end
+    end
+end
+
+size(x::DistMultiVec) = (5,)
+
+# matrices.h
+for (elty, relty, ext) in ((:Float32, :Float32, :s),
+                           (:Float64, :Float64, :d), 
+                           (:Complex64, :Float32, :c),
+                           (:Complex128, :Float64, :z))
+    @eval begin
+        function gaussian(A::DistMultiVec{$elty}, m::Integer, n::Integer, mean::$elty = zero($elty), stddev::$relty = one($relty))
+            err = ccall(($(string("ElGaussianDistMultiVec_", ext)), libEl), Cuint,
+                (Ptr{Void}, ElInt, ElInt, $elty, $relty), 
+                A.obj, m, n, mean, stddev)
+            return nothing
+        end
+    end
+end
+
+# blas_like/
+for (elty, relty, ext) in ((:Float32, :Float32, :s),
+                           (:Float64, :Float64, :d), 
+                           (:Complex64, :Float32, :c),
+                           (:Complex128, :Float64, :z))
+    @eval begin
+        function nrm2(x::DistMultiVec{$elty})
+            nm = Ref{$relty}(0)
+            err = ccall(($(string("ElNrm2DistMultiVec_", ext)), libEl), Cuint,
+                (Ptr{Void}, Ref{$relty}),
+                x.obj, nm)
+            return nm[]
+        end
+    end
+end
+
+
+# lapack_like/factor.h
+immutable RegQSDCtrl{T}
+    regPrimal::T
+    regDual::T
+    alg::Cuint
+    relTol::T
+    relTolRefine::T
+    maxRefineIts::Cint
+    restart::Cint
+    progress::Cint
+end
+for (elty, ext) in ((:Float32, :s),
+                    (:Float64, :d))
+    @eval begin 
+        function RegQSDCtrl(::Type{$elty})
+            obj = RegQSDCtrl{$elty}(0, 0, 0, 0, 0, 0, 0, 0)
+            err = ccall(($(string("ElRegQSDCtrlDefault_", ext)), libEl), Cuint, 
+                (Ptr{RegQSDCtrl{$elty}},),
+                &obj)
+            return obj 
+        end
+    end
+end
+
+# Optimization/solvers.h
+immutable IPFLineSearchCtrl{T}
+    gamma::T
+    beta::T
+    psi::T
+    stepRatio::T
+    progress::Cint
+end
+for (elty, ext) in ((:Float32, :s),
+                    (:Float64, :d))
+    @eval begin
+        function IPFLineSearchCtrl(::Type{$elty})
+            obj = IPFLineSearchCtrl{$elty}(0, 0, 0, 0, 0)
+            err = ccall(($(string("ElIPFLineSearchCtrlDefault_", ext)), libEl), Cuint, 
+                (Ptr{IPFLineSearchCtrl{$elty}},),
+                 &obj)
+            return obj 
+        end
+    end
+end
+
+immutable LPAffineIPFCtrl{T}
+    primalInit::Cint
+    dualInit::Cint
+    tol::T
+    maxIts::Cint
+    centering::T
+    qsdCtrl::RegQSDCtrl{T}
+    lineSearchCtrl::IPFLineSearchCtrl{T}
+    equilibrate::Cint
+    progress::Cint
+    time::Cint
+end
+for (elty, ext) in ((:Float32, :s),
+                    (:Float64, :d))
+    @eval begin
+        function LPAffineIPFCtrl(::Type{$elty})
+            obj = LPAffineIPFCtrl{$elty}(0, 0, 0, 0, 0, RegQSDCtrl($elty), IPFLineSearchCtrl($elty), 0, 0, 0)
+            err = ccall(($(string("ElLPAffineIPFCtrlDefault_", ext)), libEl), Cuint, 
+                (Ptr{LPAffineIPFCtrl{$elty}},),
+                 &obj)
+            return obj 
+        end
+    end
+end
+
+immutable LPAffineMehrotraCtrl{T}
+    primalInit::Cint
+    dualInit::Cint
+    tol::T
+    maxIts::Cint
+    maxStepRatio::T
+    qsdCtrl::RegQSDCtrl{T}
+    outerEquil::Cint
+    innerEquil::Cint
+    scaleTwoNorm::Cint
+    basisSize::Cint
+    progress::Cint
+    time::Cint
+end
+for (elty, ext) in ((:Float32, :s),
+                    (:Float64, :d))
+    @eval begin
+        function LPAffineMehrotraCtrl(::Type{$elty})
+            obj = LPAffineMehrotraCtrl{$elty}(0, 0, 0, 0, 0, RegQSDCtrl($elty), 0, 0, 0, 0, 0, 0)
+            err = ccall(($(string("ElLPAffineMehrotraCtrlDefault_", ext)), libEl), Cuint, 
+                (Ptr{LPAffineMehrotraCtrl{$elty}},),
+                &obj)
+            return obj
+        end
+    end
+end
+
+
+immutable LPAffineCtrl{T}
+    approach::Cuint
+    ipfCtrl::LPAffineIPFCtrl{T}
+    mehrotraCtrl::LPAffineMehrotraCtrl{T}
+end
+
+for (elty, ext) in ((:Float32, :s),
+                    (:Float64, :d))
+    @eval begin
+        function LPAffineCtrl(::Type{$elty})
+            obj = LPAffineCtrl{$elty}(0, LPAffineIPFCtrl($elty), LPAffineMehrotraCtrl($elty))
+            err = ccall(($(string("ElLPAffineCtrlDefault_", ext)), libEl), Cuint, 
+                (Ptr{LPAffineCtrl{$elty}},),
+                &obj)
+            return obj
+        end
+    end
+end
+
+# lapack_like/factor.h
+# type RegQSDCtrl64
+#     regPrimal::Float64
+#     regDual::Float64
+#     alg::Cuint
+#     relTol::Float64
+#     relTolRefine::Float64
+#     maxRefineIts::Cint
+#     restart::Cint
+#     progress::Cint
+# end
+# for (elty, ext) in ((:Float64, :d),)
+#     @eval begin 
+#         function RegQSDCtrl(::Type{$elty})
+#             obj = RegQSDCtrl64(0, 0, 0, 0, 0, 0, 0, 0)
+#             oref = Ref{RegQSDCtrl64}(obj)
+#             err = ccall(($(string("ElRegQSDCtrlDefault_", ext)), libEl), Cuint, 
+#                 (Ref{RegQSDCtrl64},),
+#                 oref)
+#             return oref[] 
+#         end
+#     end
+# end
+
+# # Optimization/solvers.h
+# type IPFLineSearchCtrl64
+#     gamma::Float64
+#     beta::Float64
+#     psi::Float64
+#     stepRatio::Float64
+#     progress::Cint
+# end
+# for (elty, ext) in ((:Float64, :d),)
+#     @eval begin
+#         function IPFLineSearchCtrl(::Type{$elty})
+#             obj = IPFLineSearchCtrl64(0, 0, 0, 0, 0)
+#             oref = Ref{IPFLineSearchCtrl64}(obj)
+#             err = ccall(($(string("ElIPFLineSearchCtrlDefault_", ext)), libEl), Cuint, 
+#                 (Ref{IPFLineSearchCtrl64},),
+#                  oref)
+#             return oref[] 
+#         end
+#     end
+# end
+
+# type LPAffineIPFCtrl64
+#     primalInit::Cint
+#     dualInit::Cint
+#     tol::Float64
+#     maxIts::Cint
+#     centering::Float64
+#     qsdCtrl::RegQSDCtrl64
+#     lineSearchCtrl::IPFLineSearchCtrl64
+#     equilibrate::Cint
+#     progress::Cint
+#     time::Cint
+# end
+# for (elty, ext) in ((:Float64, :d),)
+#     @eval begin
+#         function LPAffineIPFCtrl(::Type{$elty})
+#             obj = LPAffineIPFCtrl64(0, 0, 0, 0, 0, RegQSDCtrl($elty), IPFLineSearchCtrl($elty), 0, 0, 0)
+#             oref = Ref{LPAffineIPFCtrl64}(obj)
+#             err = ccall(($(string("ElLPAffineIPFCtrlDefault_", ext)), libEl), Cuint, 
+#                 (Ref{LPAffineIPFCtrl64},),
+#                  oref)
+#             return oref[]
+#         end
+#     end
+# end
+
+# type LPAffineMehrotraCtrl64
+#     primalInit::Cint
+#     dualInit::Cint
+#     tol::Float64
+#     maxIts::Cint
+#     maxStepRatio::Float64
+#     qsdCtrl::RegQSDCtrl64
+#     outerEquil::Cint
+#     innerEquil::Cint
+#     scaleTwoNorm::Cint
+#     basisSize::Cint
+#     progress::Cint
+#     time::Cint
+# end
+# for (elty, ext) in ((:Float64, :d),)
+#     @eval begin
+#         function LPAffineMehrotraCtrl(::Type{$elty})
+#             obj = LPAffineMehrotraCtrl64(0, 0, 0, 0, 0, RegQSDCtrl($elty), 0, 0, 0, 0, 0, 0)
+#             oref = Ref{LPAffineMehrotraCtrl64}(obj)
+#             err = ccall(($(string("ElLPAffineMehrotraCtrlDefault_", ext)), libEl), Cuint, 
+#                 (Ref{LPAffineMehrotraCtrl64},),
+#                 oref)
+#             return oref[]
+#         end
+#     end
+# end
+
+
+# type LPAffineCtrl64
+#     approach::Cuint
+#     ipfCtrl::LPAffineIPFCtrl64
+#     mehrotraCtrl::LPAffineMehrotraCtrl64
+# end
+
+# for (elty, ext) in ((:Float64, :d),)
+#     @eval begin
+#         function LPAffineCtrl(::Type{$elty})
+#             obj = LPAffineCtrl64(0, LPAffineIPFCtrl($elty), LPAffineMehrotraCtrl($elty))
+#             oref = Ref{LPAffineCtrl64}(obj)
+#             err = ccall(($(string("ElLPAffineCtrlDefault_", ext)), libEl), Cuint, 
+#                 (Ref{LPAffineCtrl64},),
+#                 oref)
+#             return oref[]
+#         end
+#     end
+# end
+
+# optimization/models.h
+for (elty, ext) in ((:Float32, :s),
+                    (:Float64, :d))
+    @eval begin
+
+        function lav(A::DistSparseMatrix{$elty}, b::DistMultiVec{$elty})
+            @show x = DistMultiVec($elty, comm(A))
+            err = ccall(($(string("ElLAVDistSparse_", ext)), libEl), Cuint, 
+                (Ptr{Void}, Ptr{Void}, Ptr{Void}),
+                A.obj, b.obj, x.obj)
+            return x
+        end
+
+        function lav(A::DistSparseMatrix{$elty}, b::DistMultiVec{$elty}, ctrl::LPAffineCtrl{$elty})
+            x = DistMultiVec($elty, comm(A))
+            err = ccall(($(string("ElLAVXDistSparse_", ext)), libEl), Cuint, 
+                (Ptr{Void}, Ptr{Void}, Ptr{Void}, Ptr{LPAffineCtrl{$elty}}),
+                A.obj, b.obj, x.obj, &ctrl)
+            return x
+        end
+    end
+end
 
 end # module
 
--- test/distributedsparse.jl
+++ test/distributedsparse.jl
@@ -1,7 +1,85 @@
 using Elemental
 
-A = Elemental.DistSparseMatrix(Float64, 3, 3)
+using MPI
 
-@show A
+n0 = 50
+n1 = 50
+display = true
+worldRank = MPI.Comm_rank(MPI.COMM_WORLD)
+
+function stackedFD2D(n0, n1)
+    height = 2*n0*n1
+    width = n0*n1
+    A = Elemental.DistSparseMatrix(Float64, height, width)
+    localHeight = Elemental.localHeight(A)
+    Elemental.reserve(A, 6*localHeight)
+    for sLoc in 0:localHeight - 1
+        s = Elemental.globalRow(A, sLoc)
+        if s < n0*n1
+            x0 = s % n0
+            x1 = div(s, n0)
+            Elemental.queueLocalUpdate(A, sLoc, s, 11.0)
+            if x0 > 0
+                Elemental.queueLocalUpdate(A, sLoc, s - 1, -10.0)
+            end
+            if x0 + 1 < n0
+                Elemental.queueLocalUpdate(A, sLoc, s + 1, 20.0)
+            end
+            if x1 > 0
+                Elemental.queueLocalUpdate(A, sLoc, s - n0, -30.0)
+            end
+            if x1 + 1 < n1
+                Elemental.queueLocalUpdate(A, sLoc, s + n0, 40.0)
+            end
+        else
+            sRel = s - n0*n1
+            x0 = sRel % n0
+            x1 = div(sRel, n0)
+            Elemental.queueLocalUpdate(A, sLoc, sRel, -20.0)
+            if x0 > 0
+                Elemental.queueLocalUpdate(A, sLoc, sRel - 1, -1.0)
+            end
+            if x0 + 1 < n0
+                Elemental.queueLocalUpdate(A, sLoc, sRel + 1, -2.0)
+            end
+            if x1 > 0
+                Elemental.queueLocalUpdate(A, sLoc, sRel - n0, -3.0)
+            end
+            if x1 + 1 < n1
+                Elemental.queueLocalUpdate(A, sLoc, sRel + n0, 3.0)
+            end
+        end
+
+        # The dense last column
+        Elemental.queueLocalUpdate(A, sLoc, width - 1, -div(10.0, height))
+
+        Elemental.makeConsistent(A)
+    end
+    return A
+end
+
+A = stackedFD2D(n0, n1)
+@show size(A)
+b = Elemental.DistMultiVec(Float64)
+Elemental.gaussian(b, 2*n0*n1, 1)
+
+@show bTwoNorm = Elemental.nrm2(b)
+
+# if display
+    # show(IO, A)
+# end
+ctrl = Elemental.LPAffineCtrl(Float64)
+unsafe_store!(convert(Ptr{Cint}, pointer_from_objref(ctrl.mehrotraCtrl.qsdCtrl.progress)), true, 1)
+unsafe_store!(convert(Ptr{Cint}, pointer_from_objref(ctrl.mehrotraCtrl.progress)), true, 1)
+unsafe_store!(convert(Ptr{Cint}, pointer_from_objref(ctrl.mehrotraCtrl.outerEquil)), true, 1)
+unsafe_store!(convert(Ptr{Cint}, pointer_from_objref(ctrl.mehrotraCtrl.time)), true, 1)
+# ctrl.mehrotraCtrl.progress = true
+# ctrl.mehrotraCtrl.outerEquil = true
+# ctrl.mehrotraCtrl.time = true
+gc()
+@show Elemental.comm(A)
+x = Elemental.lav(A, b)
+# x = Elemental.lav(A, b, ctrl)
+
 
 @show Elemental.destroy(A)
