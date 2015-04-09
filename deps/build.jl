a480eacfab2c80fb4a1a944258a71e76aad836e0
Jiahao Chen <jiahao@mit.edu>
2015-04-08 22:42:08 -0700

    Simplify build script
    Looks like nothing is done _in_ the source directory

--- deps/build.jl
+++ deps/build.jl
@@ -30,29 +30,28 @@ end
 Base.check_blas()
 blas = Base.blas_vendor()
 
-cd(srcdir) do
-    builddir = joinpath(depdir, "builds")
-    if isdir(builddir)
-        rm(builddir, recursive=true)
-    end
-    mkdir(builddir)
-
-    mathlib = Libdl.dlpath(BLAS.libblas)
-    blas64 = LinAlg.USE_BLAS64 ? "ON" : "OFF"
-    blas_suffix = blas === :openblas64 ? "_64_" : ""
-
-    cd(builddir) do
-        run(`cmake -D CMAKE_INSTALL_PREFIX=$prefix
-                   -D INSTALL_PYTHON_PACKAGE=OFF
-                   -D PYTHON_EXECUTABLE=""
-                   -D PYTHON_SITE_PACKAGES=""
-                   -D EL_USE_64BIT_INTS=$blas64
-                   -D EL_USE_64BIT_BLAS_INTS=$blas64
-                   -D MATH_LIBS=$mathlib
-                   -D EL_BLAS_SUFFIX=$blas_suffix
-                   -D EL_LAPACK_SUFFIX=$blas_suffix
-                   $srcdir`)
-        run(`make -j $CPU_CORES`)
-        run(`make install`)
-    end
+builddir = joinpath(depdir, "builds")
+if isdir(builddir)
+    rm(builddir, recursive=true)
 end
+mkdir(builddir)
+
+mathlib = Libdl.dlpath(BLAS.libblas)
+blas64 = LinAlg.USE_BLAS64 ? "ON" : "OFF"
+blas_suffix = blas === :openblas64 ? "_64_" : ""
+
+cd(builddir) do
+    run(`cmake -D CMAKE_INSTALL_PREFIX=$prefix
+               -D INSTALL_PYTHON_PACKAGE=OFF
+               -D PYTHON_EXECUTABLE=""
+               -D PYTHON_SITE_PACKAGES=""
+               -D EL_USE_64BIT_INTS=$blas64
+               -D EL_USE_64BIT_BLAS_INTS=$blas64
+               -D MATH_LIBS=$mathlib
+               -D EL_BLAS_SUFFIX=$blas_suffix
+               -D EL_LAPACK_SUFFIX=$blas_suffix
+               $srcdir`)
+    run(`make -j $CPU_CORES`)
+    run(`make install`)
+end
+
