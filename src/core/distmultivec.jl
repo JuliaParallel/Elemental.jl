88e9a6a655e03dec76e540f2cbb0338197cb0b70
Andreas Noack <andreasnoackjensen@gmail.com>
2015-11-04 17:03:29 -0500

    Fix nasty bug in DistMultiVec wrappers
    Make (*)(DistSparseMatrix, DistMultiVec) work
    
    Add a few definitions for MPICH

--- src/core/distmultivec.jl
+++ src/core/distmultivec.jl
@@ -17,6 +17,24 @@ for (elty, ext) in ((:ElInt, :i),
             return DistMultiVec{$elty}(obj[])
         end
 
+        function comm(A::DistMultiVec{$elty})
+            cm = Ref{ElComm}()
+            err = ccall(($(string("ElDistMultiVecComm_", ext)), libEl), Cuint,
+                (Ptr{Void}, Ref{ElComm}),
+                A.obj, cm)
+            err == 0 || throw(ElError(err))
+            return cm[]
+        end
+
+        function get(x::DistMultiVec{$elty}, i::Integer = size(x, 1), j::Integer = 1)
+            v = Ref{$elty}()
+            err = ccall(($(string("ElDistMultiVecGet_", ext)), libEl), Cuint,
+                (Ptr{Void}, ElInt, ElInt, Ref{$elty}),
+                x.obj, i - 1, j - 1, v)
+            err == 0 || throw(ElError(err))
+            return v[]
+        end
+
         function height(x::DistMultiVec{$elty})
             i = Ref{ElInt}()
             err = ccall(($(string("ElDistMultiVecHeight_", ext)), libEl), Cuint,
@@ -24,6 +42,37 @@ for (elty, ext) in ((:ElInt, :i),
                 x.obj, i)
             err == 0 || throw(ElError(err))
             return i[]
+        end
+
+        function processQueues(A::DistMultiVec{$elty})
+            err = ccall(($(string("ElDistMultiVecProcessQueues_", ext)), libEl), Cuint,
+                (Ptr{Void},), A.obj)
+            err == 0 || throw(ElError(err))
+          return nothing
+        end
+
+        function queueUpdate(A::DistMultiVec{$elty}, i::Integer, j::Integer, value::$elty)
+            err = ccall(($(string("ElDistMultiVecQueueUpdate_", ext)), libEl), Cuint,
+              (Ptr{Void}, ElInt, ElInt, $elty),
+              A.obj, i - 1, j - 1, value)
+            err == 0 || throw(ElError(err))
+            return nothing
+        end
+
+        function reserve(A::DistMultiVec{$elty}, numEntries::Integer)
+            err = ccall(($(string("ElDistMultiVecReserve_", ext)), libEl), Cuint,
+              (Ptr{Void}, ElInt),
+              A.obj, numEntries)
+            err == 0 || throw(ElError(err))
+            return nothing
+        end
+
+        function resize!(A::DistMultiVec{$elty}, m::Integer, n::Integer = 1) # to mimic vector behavior
+            err = ccall(($(string("ElDistMultiVecResize_", ext)), libEl), Cuint,
+              (Ptr{Void}, ElInt, ElInt),
+              A.obj, ElInt(m), ElInt(n))
+            err == 0 || throw(ElError(err))
+            return A
         end
 
         function width(x::DistMultiVec{$elty})
@@ -34,41 +83,15 @@ for (elty, ext) in ((:ElInt, :i),
             err == 0 || throw(ElError(err))
             return i[]
         end
-
-        function reserve{$elty}(A::DistMultiVec{$elty}, numEntries::Integer)
-            err = ccall(($(string("ElDistMultiVecReserve_", ext)), libEl), Cuint,
-              (Ptr{Void}, ElInt),
-              A.obj, numEntries)
-            err == 0 || throw(ElError(err))
-            return nothing
-        end
-
-        function resize!{$elty}(A::DistMultiVec{$elty}, m::Integer, n::Integer = 1) # to mimic vector behavior
-            err = ccall(($(string("ElDistMultiVecResize_", ext)), libEl), Cuint,
-              (Ptr{Void}, ElInt, ElInt),
-              A.obj, ElInt(m), ElInt(n))
-            err == 0 || throw(ElError(err))
-            return A
-        end
-
-        function queueUpdate{$elty}(A::DistMultiVec{$elty}, i::Integer, j::Integer, value::$elty)
-            err = ccall(($(string("ElDistMultiVecQueueUpdate_", ext)), libEl), Cuint,
-              (Ptr{Void}, ElInt, ElInt, $elty),
-              A.obj, i-1, j-1, value)
-            err == 0 || throw(ElError(err))
-            return nothing
-        end
-
-        function processQueues{$elty}(A::DistMultiVec{$elty})
-          err = ccall(($(string("ElDistMultiVecProcessQueues_", ext)), libEl), Cuint,
-            (Ptr{Void},), A.obj)
-          err == 0 || throw(ElError(err))
-          return nothing
-        end
     end
 end
 
-# size(x::DistMultiVec) = (Int(height(x)),) # We consider everything 2D
+#########################
+### Julia convenience ###
+#########################
+
+getindex(x::DistMultiVec, i, j) = get(x, i, j)
+
 function similar{T}(::DistMultiVec, ::Type{T}, sz::Dims, cm::ElComm = CommWorld)
     A = DistMultiVec(T, cm)
     resize!(A, sz...)
